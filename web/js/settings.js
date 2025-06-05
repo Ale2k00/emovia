b79b49adff26f58d42ca08e1686129574779326b
Ale2k00 <airbuyers@gmail.com>
2025-06-05 23:06:35 +0200

    Add dashboard and upload pages with AWS config

---
+++ web/js/settings.js
@@ -0,0 +1,8 @@
+window.oidcSettings = {
+  authority: 'https://cognito-idp.eu-central-1.amazonaws.com/eu-central-1_OiiOjtfVJ',
+  client_id: '5lvvggeja9xxxxxxxxxxxxxx',
+  redirect_uri: 'https://main.df360ms0i6ccd.amplifyapp.com/callback.html',
+  post_logout_redirect_uri: 'https://main.df360ms0i6ccd.amplifyapp.com/',
+  response_type: 'code',
+  scope: 'openid profile'
+};
